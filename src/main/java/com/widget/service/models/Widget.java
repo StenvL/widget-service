585a8baf18f39082906bb1f642a9e424badbe3db
Valeriy <sten.vl2010@gmail.com>
2020-03-29 23:21:39 +0500

    Add lombok lib and plugin.

--- src/main/java/com/widget/service/models/Widget.java
+++ src/main/java/com/widget/service/models/Widget.java
@@ -1,76 +1,23 @@
 package com.widget.service.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.ZonedDateTime;
 
 @Entity
+@Getter @Setter
 public class Widget {
     @Id
     @GeneratedValue
     private Long id;
-
     private int x;
     private int y;
     private int z;
     private double width;
     private double height;
     private ZonedDateTime lastModified;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public void setZ(int z) {
-        this.z = z;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public ZonedDateTime getLastModified() {
-        return lastModified;
-    }
-
-    public void setLastModified(ZonedDateTime lastModified) {
-        this.lastModified = lastModified;
-    }
 }
