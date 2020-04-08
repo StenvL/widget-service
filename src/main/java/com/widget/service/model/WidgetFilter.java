204e1cc921c312b53af45191efff5d79d5131bc1
Valeriy <sten.vl2010@gmail.com>
2020-04-08 21:07:40 +0500

    AllArgsConstructor for WidgetFilter

--- src/main/java/com/widget/service/model/WidgetFilter.java
+++ src/main/java/com/widget/service/model/WidgetFilter.java
@@ -1,10 +1,12 @@
 package com.widget.service.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class WidgetFilter {
     private Integer x1;
     private Integer y1;
