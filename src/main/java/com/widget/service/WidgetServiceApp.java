b72d5d7d65c1999f65fa4292cf4c49604f9b13d9
Valeriy <sten.vl2010@gmail.com>
2020-04-15 23:22:04 +0500

    Add packages to application initializer

--- src/main/java/com/widget/service/WidgetServiceApp.java
+++ src/main/java/com/widget/service/WidgetServiceApp.java
@@ -3,7 +3,10 @@ package com.widget.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={
+	"com.widget.service",
+	"com.widget.storage"
+})
 public class WidgetServiceApp {
 
 	public static void main(String[] args) {
