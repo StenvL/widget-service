3fdb7e398fbeefb1ead76cd90c2ac759ef4df641
Valeriy <sten.vl2010@gmail.com>
2020-04-17 00:42:13 +0500

    Added tests for service

--- src/test/java/com/widget/service/service/WidgetServiceTest.java
+++ src/test/java/com/widget/service/service/WidgetServiceTest.java
@@ -2,23 +2,20 @@ package com.widget.service.service;
 
 import com.widget.service.model.Widget;
 import com.widget.service.model.WidgetFilter;
-import com.widget.service.repository.WidgetRepository;
-import org.hibernate.ObjectNotFoundException;
+import com.widget.storage.EntityNotFoundException;
+import com.widget.storage.abstraction.IStorage;
+import com.widget.storage.contract.PageRequest;
+import com.widget.storage.contract.PageResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.awt.print.PageFormat;
-import java.awt.print.Pageable;
-import java.awt.print.Printable;
-import java.util.Collection;
-import java.util.stream.StreamSupport;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,58 +23,80 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class WidgetServiceTest {
     @Autowired
-    private WidgetRepository widgetRepository;
+    private IStorage storage;
 
     @Autowired
     private WidgetService widgetService;
 
+    @BeforeEach
+    public void init() {
+        storage.deleteAll(Widget.class);
+    }
+
     @Test
-    void whenGetAllWidgets_thenReturnListOfWidgets() {
+    void getAllWidgets_WithPaging_ReturnWidgetsPage() {
         // given
-        widgetRepository.deleteAll();
+        Widget widget1 = getWidget();
+        Widget widget2 = getWidget();
+        Widget widget3 = getWidget();
+        widget1.setZ(3);
+        widget2.setZ(2);
+        widget3.setZ(1);
+
+        storage.saveAll(new ArrayList<>(List.of(widget1, widget2, widget3)));
+
+        // when
+        PageResponse widgets = widgetService.getAllWidgets(new PageRequest(1, 2), null);
+
+        // then
+        int[] expected = { 1 };
+        assertArrayEquals(
+                widgets.getRecords().stream().mapToInt(x -> ((Widget)x).getZ()).toArray(),
+                expected);
+    }
+
+
+    @Test
+    void getAllWidgets_WithPagingAndFiltering_ReturnsFilteredWidgetsPage() {
+        // given
         Widget widget1 = new Widget();
         widget1.setX(10);
         widget1.setY(10);
         widget1.setWidth(5);
         widget1.setHeight(5);
-        widgetRepository.save(widget1);
+        widgetService.createWidget(widget1);
 
         Widget widget2 = new Widget();
         widget2.setX(25);
         widget2.setY(25);
         widget2.setWidth(5);
         widget2.setHeight(5);
-        widgetRepository.save(widget2);
+        widgetService.createWidget(widget2);
 
         WidgetFilter filter1 = new WidgetFilter(0, 0, 15, 15);
         WidgetFilter filter2 = new WidgetFilter(20, 20, 35, 35);
         WidgetFilter filter3 = new WidgetFilter(10, 10, 25, 25);
-        PageRequest pageRequest = PageRequest.of(0, 10);
+        PageRequest pageRequest = new PageRequest(0, 10);
 
         // when
-        Iterable<Widget> res1 = widgetService.getAllWidgets(pageRequest, filter1);
-        Iterable<Widget> res2 = widgetService.getAllWidgets(pageRequest, filter2);
-        Iterable<Widget> res3 = widgetService.getAllWidgets(pageRequest, filter3);
-        Iterable<Widget> res4 = widgetService.getAllWidgets(pageRequest, null);
+        PageResponse res1 = widgetService.getAllWidgets(pageRequest, filter1);
+        PageResponse res2 = widgetService.getAllWidgets(pageRequest, filter2);
+        PageResponse res3 = widgetService.getAllWidgets(pageRequest, filter3);
+        PageResponse res4 = widgetService.getAllWidgets(pageRequest, null);
 
         // then
-        assertEquals(StreamSupport.stream(res1.spliterator(), false).count(), 1);
-        assertEquals(StreamSupport.stream(res2.spliterator(), false).count(), 1);
-        assertEquals(StreamSupport.stream(res3.spliterator(), false).count(), 0);
-        assertEquals(StreamSupport.stream(res4.spliterator(), false).count(), 2);
+        assertEquals(res1.getRecords().size(), 1);
+        assertEquals(res2.getRecords().size(), 1);
+        assertEquals(res3.getRecords().size(), 0);
+        assertEquals(res4.getRecords().size(), 2);
     }
 
     @Test
-    void whenGetWidgetById_thenReturnWidget() {
+    void getWidgetById_ExistingWidget_ReturnsWidget() {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetRepository.save(widget1);
-
-        long id = widget1.getId();
+        Widget widgetToSave = getWidget();
+        storage.save(widgetToSave);
+        UUID id = widgetToSave.getId();
 
         // when
         Widget loadedWidget = widgetService.getWidgetById(id);
@@ -87,112 +106,97 @@ class WidgetServiceTest {
     }
 
     @Test
-    void whenCreateWidget_thenWidgetExistsInRepository() {
+    void createWidget_ItExistsInRepository() {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetService.createWidget((widget1));
-
-        long id = widget1.getId();
+        Widget widgetToCreate = getWidget();
+        widgetService.createWidget(widgetToCreate);
+        UUID id = widgetToCreate.getId();
 
         // when
-        boolean isExists = widgetRepository.existsById(id);
+        boolean isExists = storage.exists(Widget.class, id);
 
         // then
         assertTrue(isExists);
     }
 
     @Test
-    void whenCreateWidgetWithExistentZIndex_thenItBecomesUnique() {
+    void createWidget_ExistentZIndex_ItBecomesUnique() {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
+        Widget widget1 = getWidget();
         widget1.setZ(1);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetService.createWidget((widget1));
-        long widget1Id = widget1.getId();
+        widgetService.createWidget(widget1);
+        UUID widget1Id = widget1.getId();
 
-        Widget widget2 = new Widget();
-        widget2.setX(10);
-        widget2.setY(10);
+        Widget widget2 = getWidget();
         widget2.setZ(1);
-        widget2.setWidth(5);
-        widget2.setHeight(5);
 
         // when
         widgetService.createWidget(widget2);
 
         // then
-        assertTrue(widgetRepository.findById(widget1Id).get().getZ() == 2 && widget2.getZ() == 1);
+        assertTrue(storage.findById(Widget.class, widget1Id).get().getZ() == 2 && widget2.getZ() == 1);
     }
 
     @Test
-    void whenModifyWidget_thenItUpdates() {
+    void modifyWidget_ItUpdates() throws EntityNotFoundException {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetRepository.save((widget1));
-
-        long id = widget1.getId();
+        Widget widgetToCreate = getWidget();
+        widgetService.createWidget(widgetToCreate);
+        UUID id = widgetToCreate.getId();
 
         // when
-        Widget widgetBeforeUpdate = widgetRepository.findById(id).get();
-        int xBeforeUpdate = widgetBeforeUpdate.getX();
+        Widget widgetBeforeUpdate = storage.findById(Widget.class, id).get();
+        assertEquals(widgetBeforeUpdate.getX(), 10);
 
         widgetBeforeUpdate.setX(20);
-        widgetService.modifyWidget(widget1);
-        Widget widgetAfterUpdate = widgetRepository.findById(id).get();
-        int xAfterUpdate = widgetBeforeUpdate.getX();
+        widgetService.modifyWidget(widgetBeforeUpdate);
+        int xAfterUpdate = storage.findById(Widget.class, id).get().getX();
 
         // then
-        assertTrue(xBeforeUpdate == 10 && xAfterUpdate == 20);
+        assertEquals(xAfterUpdate, 20);
     }
 
     @Test
-    void whenModifyNonexistentWidget_thenThrows() {
+    void modifyWidget_NonExistentWidget_ThrowsEntityNotFoundException() throws EntityNotFoundException {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetRepository.save((widget1));
+        Widget widgetToSave = getWidget();
+        storage.save(widgetToSave);
+        assertTrue(storage.exists(Widget.class, widgetToSave.getId()));
 
         // when
-        widgetRepository.delete(widget1);
-        widget1.setX(20);
+        storage.delete(widgetToSave);
+        widgetToSave.setX(20);
 
         // then
-        assertThrows(ObjectNotFoundException.class, () -> {
-            widgetService.modifyWidget(widget1);
+        assertThrows(EntityNotFoundException.class, () -> {
+            widgetService.modifyWidget(widgetToSave);
         });
     }
 
     @Test
-    void deleteWidget() {
+    void deleteWidget_ItDisappeared() throws EntityNotFoundException {
         // given
-        Widget widget1 = new Widget();
-        widget1.setX(10);
-        widget1.setY(10);
-        widget1.setWidth(5);
-        widget1.setHeight(5);
-        widgetRepository.save((widget1));
-        long id = widget1.getId();
+        Widget widget1 = getWidget();
+        storage.save(widget1);
+        UUID id = widget1.getId();
 
-        assertTrue(widgetRepository.existsById(id));
+        assertTrue(storage.exists(Widget.class, id));
 
         // when
         widgetService.deleteWidget(id);
 
         // then
-        assertFalse(widgetRepository.existsById(id));
+        assertFalse(storage.exists(Widget.class, id));
+    }
+
+
+    private Widget getWidget() {
+        Widget widget = new Widget();
+        widget.setX(10);
+        widget.setY(10);
+        widget.setWidth(5);
+        widget.setHeight(5);
+
+        return widget;
     }
 }
